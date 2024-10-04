a03c08864932675afbc73000a1b72686bcb8ee02
Insomnium1250 <quang_evilrocker@yahoo.com.vn>
2024-10-05 00:03:29 +0700

    completed exercise 06

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  if (typeof string !== 'string') {
+    return 'ERROR';
+  }
+ return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+  if (Number.isInteger(num1) == false || Number.isInteger(num2) == false || num1 < 0 || num2 < 0) {
+    return "ERROR";
+  };
+  let result = (num1+num2)*(Math.abs(num2-num1)+1)/2;
+  return result;
 };
 
 // Do not edit below this line
