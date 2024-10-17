42aab2f813ef9a420bbf6c5bc054a0d76b6402b5
Insomnium1250 <quang_evilrocker@yahoo.com.vn>
2024-10-17 14:00:49 +0700

    Exercise 10 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const arr = str
+  .toLowerCase()
+  .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+  .split("")
+  .filter((char) => char !== " ");
+  const arrReverse = [...arr].reverse();
+  if (arr.toString() === arrReverse.toString()) {
+    return true;
+  } else {
+    return false;
+  }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
